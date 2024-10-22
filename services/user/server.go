8b0d83b045fd8d6b81a1fdcae52373aaa42c37de
yang-youngjun <dasd412@naver.com>
2024-10-22 11:44:02 +0900

    유저 서비스에서 프로토버퍼 사용

--- services/user/server.go
+++ services/user/server.go
@@ -1,5 +1,16 @@
 package user
 
-func main() {
+import (
+	"log"
+	msasubmoduleci "user/pkg/proto/userpb/userpb"
+)
 
+func main() {
+	user := msasubmoduleci.User{
+		Id:        "1",
+		LastName:  "test",
+		FirstName: "test",
+		Age:       31,
+	}
+	log.Println(user.Id, user.FirstName, user.LastName, user.Age)
 }
